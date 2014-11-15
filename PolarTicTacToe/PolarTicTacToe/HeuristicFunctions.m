3a03b9b598c107a9f4b014f912327fa19ef1fedc
Joshua Leger <legermjoshua@gmail.com>
2014-11-15 10:57:18 -0700

    Added win checking to heuristic.

--- PolarTicTacToe/PolarTicTacToe/HeuristicFunctions.m
+++ PolarTicTacToe/PolarTicTacToe/HeuristicFunctions.m
@@ -11,136 +11,179 @@
 
 @implementation HeuristicFunctions
 
-//Value will center around 0. 999 means that team won, -999 means the team lost. negative numbers mean the other team is doing better.
+//Value will center around 0. 999 means the team won, -999 means the team lost. negative numbers mean the other team is doing better.
 +(int) getValue:(int *)gameBoard :(int)team
 {
     int value = 0;
 
-    int winner = 0;//[Utilities teamWon:gameBoard];
+    int winner = 0;
 
-    //Check to see if anybody has won yet
-    if(winner == 1)
-    {
-        value = 999;
-    }
-    else if(winner == 2)
-    {
-        value = -999;
-    }
-    //Nobody has won yet, so look for how many unrestricted lines each player has and determine who is in better shape from there
     //Look for how many unrestricted lines each player has and determine who is in better shape from there
-    else
-    {
-        int k, a, t;
+    int k, a, t;
 
-    	//Check for vertical lines
-	    for(k = 0; k < 12; k++)
-	    {
-	        int x = 1;
-	        int y = 1;
-	        for(a = 0; a < 4; a++)
-	        {
-	            if(gameBoard[k*4 + a] == 1)
-	            {
-	                y = 0;
-	            }
-	            else if(gameBoard[k*4 + a] == 2)
-	            {
-	                x = 0;
-	            }
-	        }
-	
-	        if(x && !y)
-	        {
-	            value++;
-	        }
-	        else if(!x && y)
-	        {
-	            value--;
-	        }
-	    }
-	
-	    //Check for diagonal lines
-	    for(k = 0; k < 12; k++)
-	    {
-	        int x = 1;
-	        int y = 1;
-	        for(a = 0; a < 4; a++)
-	        {
-	                if(gameBoard[((k + a) % 12)*4 + a] == 1)
-	                {
-	                    y = 0;
-	                }
-	                else if(gameBoard[((k + a) % 12)*4 + a] == 2)
-	                {
-	                    x = 0;
-	                }
-	        }
-	
-	        if(x && !y)
-	        {
-	            value++;
-	        }
-	        else if(!x && y)
-	        {
-	            value--;
-	        }
-	
-	        x = 1;
-	        y = 1;
-	        for(a = 0; a < 4; a++)
-	        {
-	                if(gameBoard[((12 + k - a) % 12)*4 + a] == 1)
-	                {
-	                    y = 0;
-	                }
-	                else if(gameBoard[((12 + k - a) % 12)*4 + a] == 2)
-	                {
-	                    x = 0;
-	                }
-	        }
-	
-	        if(x && !y)
-	        {
-	            value++;
-	        }
-	        else if(!x && y)
-	        {
-	            value--;
-	        }
-	        
-	    }
-	
-	    //Check for horizontal
-	    for(a = 0; a < 4; a++)
-	    {
-	        for(k = 0; k < 12; k++)
-	        {
-	            int x = 1;
-	            int y = 1;
-	
-	            for(t = 0; t < 4; t++)
-	            {
-	                if(gameBoard[((k + t) % 12)*4 + a] == 1)
-	                {
-	                    y = 0;
-	                }
-	                else if(gameBoard[((k + t) % 12)*4 + a] == 2)
-	                {
-	                    x = 0;
-	                }
-	            }
-	
-	            if(x && !y)
-	            {
-	                value++;
-	            }
-	            else if(!x && y)
-	            {
-	                value--;
-	            }
-	        }
-	    }
+    //Check for vertical lines
+    for(k = 0; k < 12; k++)
+    {
+        int x = 1;
+        int y = 1;
+        int winX = 0;
+        int winY = 0;
+        for(a = 0; a < 4; a++)
+        {
+            if(gameBoard[k*4 + a] == 1)
+            {
+                y = 0;
+                winX++;
+            }
+            else if(gameBoard[k*4 + a] == 2)
+            {
+                x = 0;
+                winY++;
+            }
+        }
+
+        if(winX == 4)
+        {
+            winner = 1;
+            value = 999;
+            break;
+        }
+        else if(winY == 4)
+        {
+            winner = 1;
+            value = -999;
+            break;
+        }
+
+        if(x && !y)
+        {
+            value++;
+        }
+        else if(!x && y)
+        {
+            value--;
+        }
+    }
+
+    if(winner == 0)
+    {
+        //Check for diagonal lines
+        for(k = 0; k < 12; k++)
+        {
+            int x = 1;
+            int y = 1;
+            int winX = 0; 
+            int winY = 0;
+            for(a = 0; a < 4; a++)
+            {
+                    if(gameBoard[((k + a) % 12)*4 + a] == 1)
+                    {
+                        y = 0;
+                        winX++;
+                    }
+                    else if(gameBoard[((k + a) % 12)*4 + a] == 2)
+                    {
+                        x = 0;
+                        winY++;
+                    }
+            }
+    
+            if(x && !y)
+            {
+                value++;
+            }
+            else if(!x && y)
+            {
+                value--;
+            }
+    
+            x = 1;
+            y = 1;
+            for(a = 0; a < 4; a++)
+            {
+                    if(gameBoard[((12 + k - a) % 12)*4 + a] == 1)
+                    {
+                        y = 0;
+                    }
+                    else if(gameBoard[((12 + k - a) % 12)*4 + a] == 2)
+                    {
+                        x = 0;
+                    }
+            }
+    
+            if(winX == 4)
+            {
+                winner = 1;
+                value = 999;
+                break;
+            }
+            else if(winY == 4)
+            {
+                winner = 1;
+                value = -999;
+                break;
+            }
+
+            if(x && !y)
+            {
+                value++;
+            }
+            else if(!x && y)
+            {
+                value--;
+            }
+        }
+    }
+
+    if(winner == 0)
+    {
+        //Check for horizontal
+        for(a = 0; a < 4; a++)
+        {
+            for(k = 0; k < 12; k++)
+            {
+                int x = 1;
+                int y = 1;
+                int winX = 0;
+                int winY = 0;
+    
+                for(t = 0; t < 4; t++)
+                {
+                    if(gameBoard[((k + t) % 12)*4 + a] == 1)
+                    {
+                        y = 0;
+                        winX++;
+                    }
+                    else if(gameBoard[((k + t) % 12)*4 + a] == 2)
+                    {
+                        x = 0;
+                        winY++;
+                    }
+                }
+
+                if(winX == 4)
+                {
+                    winner = 1;
+                    value = 999;
+                    break;
+                }
+                else if(winY == 4)
+                {
+                    winner = 1;
+                    value = -999;
+                    break;
+                }
+    
+                if(x && !y)
+                {
+                    value++;
+                }
+                else if(!x && y)
+                {
+                    value--;
+                }
+            }
+        }
     }
     
     if(team == 2)
